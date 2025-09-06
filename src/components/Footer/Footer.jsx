743441924507538ae247b7dad63ebb9f488799f5
AndreyChiz <andrey.chizhov.dev@gmail.com>
2025-09-06 14:57:50 +0400

    📦 refactor(app): add base markdown

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+
+export default function Footer() {
+
+    return (
+        <footer className={styles.footer}>
+            <h1 className={styles.some}>It is Footer</h1>
+            
+        </footer>
+    )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css'
+
+
+export default function Header() {
+
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.some}>It is header</h1>
+        </header>
+    )
+}
