efc498c18797957a33436c89667e02e8a9040f22
AndreyChiz <andrey.chizhov.dev@gmail.com>
2025-09-06 15:32:03 +0400

    ✨ feat(header): add base markdawn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,22 @@
 import styles from './Header.module.css'
 
-
 export default function Header() {
-
     return (
         <header className={styles.header}>
-            <h1 className={styles.some}>It is header</h1>
+            <div className={styles.logo}>
+                <h1>Мой сайт</h1>
+            </div>
+
+            <nav className={styles.menu}>
+                <a href="#blog">Блог</a>
+                <a href="#lifestyle">Лайфстайл</a>
+                <a href="#projects">Проекты</a>
+            </nav>
+
+            <div className={styles.auth}>
+                <button className={styles.login}>Войти</button>
+                <button className={styles.register}>Зарегистрироваться</button>
+            </div>
         </header>
     )
 }
--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -28,6 +28,78 @@ export default function MainContent() {
         voluptates cumque, labore ratione natus exercitationem repellat deleniti
         possimus autem consequatur.
       </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
+          <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus numquam,
+              mollitia doloribus illo cumque culpa. Error reprehenderit amet omnis
+              voluptates cumque, labore ratione natus exercitationem repellat deleniti
+              possimus autem consequatur.
+          </section>
     </main>
   )
 }
