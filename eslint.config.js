794f85514760352254e5adda19e7c85ce0e8a5be
AndreyChiz <andrey.chizhov.dev@gmail.com>
2025-09-09 20:41:15 +0400

    🛠 build(dependency): install preettier eslint & setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -7,14 +8,20 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     extends: [
       js.configs.recommended,
+      react.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -24,6 +31,13 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // можно добавить правила React, если нужно:
+      'react/react-in-jsx-scope': 'off', // React 17+ не требует import React
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ])
---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+    tabWidth: 4,
+    trailingComma: 'all',
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    arrowParens: 'always',
+};
