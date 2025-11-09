697cbd37586951a2e9040c019ffadd4347b0e860
Andrey Chizhov <Achizhovjob@yandex.ru>
2025-11-09 10:28:18 +0400

    🦄 refactor(styles): add styles by import

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,14 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 
 import './assets/css/bootstrap.min.css'
+import './assets/css/jquery.fancybox.min.css'
+import './assets/css/owl.carousel.min.css'
+import './assets/css/owl.theme.default.min.css'
+import './assets/css/aos.css'
+import './assets/css/style.css'
+import './assets/fonts/icomoon/style.css'
+import './assets/fonts/flaticon/font/flaticon.css'
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
